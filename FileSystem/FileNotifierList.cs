991a3176de0859773996881f95de19f538de23f7
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2023-02-12 11:35:29 +0100

    Switch to AsyncKeyedLock, which will fix a race condition in this code.

--- FileSystem/FileNotifierList.cs
+++ FileSystem/FileNotifierList.cs
@@ -1,34 +1,35 @@
-using Plugin;
-using System.Collections.Concurrent;
-using System.Threading;
+using AsyncKeyedLock;
 
 public class FileNotifierList
 {
-    private ConcurrentDictionary<string, SemaphoreSlim> _dict;
-    private IFsNotifyCatcher _catcher;
+    private readonly AsyncKeyedLocker<string> _asyncKeyedLocker;
+    private readonly IFsNotifyCatcher _catcher;
     public FileNotifierList(IFsNotifyCatcher catcher) 
     {
-        _dict = new ConcurrentDictionary<string, SemaphoreSlim>();
+        _asyncKeyedLocker = new(o =>
+        {
+            o.PoolSize = 20;
+            o.PoolInitialFill = 1;
+        });
         _catcher = catcher;
     }
 
     public async Task Opened(FileSystem.IrnFile file)
     {
         _catcher.Notify(new NotifyOpened{File = file});
-        var semaphore = _dict.GetOrAdd(file.RelativePath(), (_relativePath) => new SemaphoreSlim(1));
-        await semaphore.WaitAsync();
+        var semaphore = _asyncKeyedLocker.GetOrAdd(file.RelativePath());
+        await semaphore.SemaphoreSlim.WaitAsync();
     }
 
     public void Closed(FileSystem.IrnFile file)
     {
-        SemaphoreSlim? semaphore;
-        var removed = _dict.TryRemove(file.RelativePath(), out semaphore);
-        semaphore?.Release();
+        if (_asyncKeyedLocker.Index.TryGetValue(file.RelativePath(), out var semaphore))
+        {
+            semaphore.Dispose();
+        }
         _catcher.Notify(new NotifyClosed{File = file});
-
-
     }
 
-    public bool IsBusy(FileSystem.IrnFile file) => _dict.ContainsKey(file.RelativePath());
+    public bool IsBusy(FileSystem.IrnFile file) => _asyncKeyedLocker.IsInUse(file.RelativePath());
 
 }
